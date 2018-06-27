e67e176c14ada4ba4c3249534c90d7759d65d811
moskowitza <moskowitzaaron@mac.com>
2018-06-26 21:23:55 -0400

    header and login component

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+  <footer className="header">
+    <span>Animal Sanctuary 2018</span>
+  </footer>
+);
+
+export default Header;
