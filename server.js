d76de52bb94468d5d4e251a1ae110fc726b4baab
Tran <Tran.Judie@gmail.com>
2018-06-27 17:21:10 -0400

    working SQL connection

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const db = require("./models");
+
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,7 +14,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+
+
+
 // Define API routes here
+
+
+
 
 // Send every other request to the React app
 // Define any API routes before this runs
@@ -20,6 +28,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+db.sequelize.sync({force: true}).then(function() {
+  app.listen(PORT, () => {
+    console.log(`🌎 ==> Server now on port ${PORT}!`);
+  });
 });
+
+
