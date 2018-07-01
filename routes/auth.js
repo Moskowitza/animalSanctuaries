223d59748240938b429ca574e3434de1451ae30b
moskowitza <moskowitzaaron@mac.com>
2018-07-01 13:19:16 -0400

    auth file

--- routes/auth.js
+++ routes/auth.js
@@ -5,24 +5,12 @@ module.exports = function (app, passport) {
     // GET routes to render our handlebar pages
     app.get('/signup', authController.signup);
     app.get('/signin', authController.signin);
-<<<<<<< HEAD
-    //WE need a logout
-=======
     //     //WE need a logout
->>>>>>> 3c6a766e7e39b1c24578d3ce6019cdb301157d97
     app.get('/logout', authController.logout);
 
     // POST route to implement passport and sign up a user
     /**Since we need passport, we need to pass it to this method. 
      * We can import passport in this script OR pass it from server.js. NOTE WE TAKE IN APP, Passport as params */
-<<<<<<< HEAD
-    app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/dashboard', //GET path defined below
-        failureRedirect: '/signup'
-    }
-    ));
-    // Our successful redirect needs a GET path. wow.
-=======
     app.post('/auth/signup', passport.authenticate('local-signup', {
         successRedirect: '/dashboard', //GET path defined below
         failureRedirect: '/signup'
@@ -42,7 +30,6 @@ module.exports = function (app, passport) {
 
 
 
->>>>>>> 3c6a766e7e39b1c24578d3ce6019cdb301157d97
     // A function to see if we're logged in to protect the routes
     //we pass it back to the dashboard get route
     function isLoggedIn(req, res, next) {
@@ -51,22 +38,14 @@ module.exports = function (app, passport) {
         res.redirect('/signin');
     }
     app.get('/dashboard', isLoggedIn, authController.dashboard);
-<<<<<<< HEAD
-    
-    app.post('/signin', passport.authenticate('local-signin', {
-=======
 
     app.post('/auth/signin', passport.authenticate('local-signin', {
->>>>>>> 3c6a766e7e39b1c24578d3ce6019cdb301157d97
         successRedirect: '/dashboard',
         failureRedirect: '/signin'
     }
     ));
-<<<<<<< HEAD
-=======
     app.get('/auth/logout',function(req,res){
         req.logout();
         res.json(true);
     })
->>>>>>> 3c6a766e7e39b1c24578d3ce6019cdb301157d97
 }
