14035065c6022cd89bf3995c3d831044e0b9bdd6
moskowitza <moskowitzaaron@mac.com>
2019-05-27 08:20:30 -0400

    config

--- config/config.js
+++ config/config.js
@@ -1,22 +1,21 @@
-require('dotenv').config();
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": "mysql2018",
-    "database": "animalSan_db",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": "false"
+  development: {
+    username: 'root',
+    password: 'mysql2018',
+    database: 'animalSan_db',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: 'false',
   },
-  "test": {
-    "username": "root",
-    "password": "mysql2018",
-    "database": "animalSan_db",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'root',
+    password: 'mysql2018',
+    database: 'animalSan_db',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    use_env_variable: 'JAWSDB_URL',
+    dialect: 'mysql',
+  },
+};
