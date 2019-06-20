c94e432f4dee52247fedeffb698b7b60b5f2b9e8
moskowitza <moskowitzaaron@mac.com>
2019-06-20 13:11:59 -0400

    select only some user data to respond

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -16,7 +16,9 @@ exports.signinpost = function (req, res) {
   // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
   // So we're sending the user back the route to the members page because the redirect will happen on the front end
   // They won't get this or even be able to access this page if they aren't authed
-  res.json(req.user);
+  const { email, firstname, lastname } = req.user;
+  const userData = { email, firstname, lastname }
+  res.json(userData);
 };
 exports.authSignup = function (req, res) {
   // console.log(req.body);
