c3b73b2b2c42bce36fea8012be5eb712e3054bb6
moskowitza <moskowitzaaron@mac.com>
2019-06-21 20:41:05 -0400

    new build

---
+++ client/build/precache-manifest.e862af84f5d324e386fd8cf68d56b6d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e504d8a199ce1901ad95d5b307597b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5dcf68cb18f99a7bf70",
+    "url": "/static/css/main.d3164de5.chunk.css"
+  },
+  {
+    "revision": "5f796c02c356bfbe1386",
+    "url": "/static/js/2.27b1371f.chunk.js"
+  },
+  {
+    "revision": "b5dcf68cb18f99a7bf70",
+    "url": "/static/js/main.ab3902db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
